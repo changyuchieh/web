c53879bc4b9ea546108ed1195921a864a6d872fb
Ashley Chang <maymay51885188@gmail.com>
2018-01-11 23:02:22 -0500

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :knowledges, only: [:index]
   resources :educates
+  
+  resources :members
 end
